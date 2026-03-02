5c1c8429bf819f2dcfdbd878bcb1456df675f228
Ismael Santos <ismaelifrr@gmail.com>
2026-03-01 21:14:09 -0400

    Update complete.py

--- core/pdf/complete.py
+++ core/pdf/complete.py
@@ -1,7 +1,6 @@
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
-
-from core.money import brl
+from core.money import brl           # <--- O problema está aqui
 from core.pdf.base import draw_header
 
 
